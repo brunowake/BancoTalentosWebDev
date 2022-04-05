d5be366f7c08619ed60bb1180c139fe94c34b72d
Bruno Wake <brunowake.bw@gmail.com>
2022-04-05 15:43:13 -0300

    criando rotas e data.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CvAdicionar from "./pages/CvAdicionar";
+import ProfileList from "./pages/ProfileList";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        {/* '/' -> profileList */}
+        <Route path="/" element={<ProfileList />} />
+        {/* /editar -> criar componente editar  */}
+        <Route path="/editar" element={<div>editar</div>} />
+        {/* /criar  */}
+        <Route path="/criar" element={<CvAdicionar />} />
+        {/* 'cv' -> criar componente de cv */}
+        <Route path="/cv/:id" element={<div>cv</div>} />
+        {/* '*' pagina de erro */}
+        <Route path="*" element={<div>erro</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
