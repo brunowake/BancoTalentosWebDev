8bdeb31d49f8ba5b42b96260dc10f0a438c27ff2
Bruno Wake <brunowake.bw@gmail.com>
2022-04-20 16:00:56 -0300

    configurando json server

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./src/db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
